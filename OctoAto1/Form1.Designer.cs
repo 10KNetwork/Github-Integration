200e05b2f3c91b90bbf7c2793207fdf779d87786
ttatotab <ttatotab@gmail.com>
2016-05-28 14:41:08 +0000

    First time upload

---
+++ OctoAto1/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace OctoAto1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.RepoList = new System.Windows.Forms.ListBox();
+            this.userName = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "List";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RepoList
+            // 
+            this.RepoList.FormattingEnabled = true;
+            this.RepoList.Location = new System.Drawing.Point(12, 26);
+            this.RepoList.Name = "RepoList";
+            this.RepoList.Size = new System.Drawing.Size(260, 134);
+            this.RepoList.TabIndex = 2;
+            // 
+            // userName
+            // 
+            this.userName.Location = new System.Drawing.Point(12, 229);
+            this.userName.Name = "userName";
+            this.userName.Size = new System.Drawing.Size(164, 20);
+            this.userName.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 226);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 213);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter collaborator user name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(209, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Generate List of repositories by clicking list.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.userName);
+            this.Controls.Add(this.RepoList);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox RepoList;
+        private System.Windows.Forms.TextBox userName;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ OctoAto1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Octokit;
+using Octokit.Internal;
+
+namespace OctoAto1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+        public async void reposhowtest()
+        {
+            var github = new GitHubClient(new ProductHeaderValue("OctoAto"),
+                new InMemoryCredentialStore(new Credentials("594a631687ce87bf18617fab4933d7176b66b682")));
+            var repos = await github.Repository.GetAllForCurrent();
+            List<string> _items = new List<string>();
+            foreach (var repository in repos)
+            {
+                    _items.Add(repository.Name);
+            }
+            RepoList.DataSource = _items;
+        }
+
+        public async void addcollab()
+        {
+            var github = new GitHubClient(new ProductHeaderValue("OctoAto"),
+                new InMemoryCredentialStore(new Credentials("594a631687ce87bf18617fab4933d7176b66b682")));
+            string repo = RepoList.GetItemText(RepoList.SelectedItem);
+            await github.Repository.Collaborator.Add("ttatotab", repo, userName.Text);
+            MessageBox.Show("Done!");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            reposhowtest();
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            addcollab();
+        }
+    }
+}
